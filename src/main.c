6e1afb3cae3a06adac846efc3a6a6716d9564792
fighter3005 <oscarkemme@googlemail.com>
2018-01-17 15:40:08 +0100

    fixed random block
    fixed the "always the same first block" issue

--- src/main.c
+++ src/main.c
@@ -13,7 +13,8 @@ int main(int argc, char *argv[])
 {
 	// Pixels per Block
 	int PpB = RESOLUTION_HEIGHT / 25;
-	
+	srand(time(NULL));
+
 	tetrisGame game;
 	tetrisGame* pointGame;
 	pointGame = &game;
@@ -21,30 +22,30 @@ int main(int argc, char *argv[])
 	game.columns = 10;
 	game.score = 0;
 	game.level = 1;
-	
+
 	int renderMap[game.rows][game.columns];
-	
+
 	tetrisGetNextBlock (pointGame);
 	tetrisGetNextBlock (pointGame);
-	
+
 	/* Init map */
-	for (int x = 0; x < game.columns; x++) 
+	for (int x = 0; x < game.columns; x++)
 	{
-		for (int y = 0; y < game.rows; y++) 
+		for (int y = 0; y < game.rows; y++)
 		{
 			game.map[y][x] = 0;
 		}
 	}
-	
-	for (int x = 0; x < game.columns; x++) 
+
+	for (int x = 0; x < game.columns; x++)
 	{
-		for (int y = 0; y < game.rows; y++) 
+		for (int y = 0; y < game.rows; y++)
 		{
 			renderMap[y][x] = 0;
 		}
 	}
-		
-	
+
+
 
 	/* Anfang Basic Framework */
 	if (SDL_Init( SDL_INIT_VIDEO| SDL_INIT_AUDIO | SDL_INIT_TIMER ) < 0)
@@ -53,10 +54,10 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-	SDL_Window* win = SDL_CreateWindow("Tetris 1984", 
+	SDL_Window* win = SDL_CreateWindow("Tetris 1984",
 										SDL_WINDOWPOS_CENTERED,
 										SDL_WINDOWPOS_CENTERED,
-										RESOLUTION_WIDTH, 
+										RESOLUTION_WIDTH,
 										RESOLUTION_HEIGHT, 0);
 	if (!win)
 	{
@@ -64,10 +65,10 @@ int main(int argc, char *argv[])
 		SDL_Quit();
 		return 1;
 	}
-	
+
 	Uint32 render_flags = SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC;
 	SDL_Renderer* rend = SDL_CreateRenderer(win, -1, render_flags);
-	
+
 	if (!rend)
 	{
 		printf("Fehler beim Initialisieren des Renderers: %s\n", SDL_GetError());
@@ -75,7 +76,7 @@ int main(int argc, char *argv[])
 		SDL_Quit();
 		return 1;
 	}
-	
+
 	SDL_Surface* BlockI = IMG_Load("textures/TexBlockI.png");
 	SDL_Surface* BlockL = IMG_Load("textures/TexBlockL.png");
 	SDL_Surface* BlockJ = IMG_Load("textures/TexBlockJ.png");
@@ -85,9 +86,9 @@ int main(int argc, char *argv[])
 	SDL_Surface* BlockZ = IMG_Load("textures/TexBlockZ.png");
 	SDL_Surface* Edges = IMG_Load("textures/Edges.png");
 	SDL_Surface* BackgroundImage = IMG_Load("textures/Background.jpg");
-	
-	
-	
+
+
+
 	if (!(BlockI || BlockL || BlockJ || BlockO || BlockS || BlockT || BlockZ || Edges || BackgroundImage))
 	{
 		printf("Fehler beim Initialisieren von mindestens einer der Oberflaechen: %s\n", SDL_GetError());
@@ -96,7 +97,7 @@ int main(int argc, char *argv[])
 		SDL_Quit();
 		return 1;
 	}
-	
+
 	SDL_Texture* texBlockI = SDL_CreateTextureFromSurface(rend, BlockI);
 	SDL_Texture* texBlockL = SDL_CreateTextureFromSurface(rend, BlockL);
 	SDL_Texture* texBlockJ = SDL_CreateTextureFromSurface(rend, BlockJ);
@@ -115,7 +116,7 @@ int main(int argc, char *argv[])
 	SDL_FreeSurface(BlockZ);
 	SDL_FreeSurface(Edges);
 	SDL_FreeSurface(BackgroundImage);
-	
+
 	/* Fehlercode an die neuen Texturen anpassen */
 	if (!(texBlockI || texBlockL || texBlockJ || texBlockS || texBlockT || texBlockO || texBlockZ || texEdges || texBackgroundImage))
 	{
@@ -126,13 +127,13 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 	/* Ende Basic Framework */
-	
+
 	/* Music */
 	Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
 	Mix_Music* BackgroundMusic = Mix_LoadMUS("music/BackgroundMusic.mp3");
 	Mix_PlayMusic(BackgroundMusic, -1);
-	
-	
+
+
 	//Struct der Position und Groesse des Sprites
 	SDL_Rect RBlockI;
 	SDL_Rect RBlockL;
@@ -142,8 +143,8 @@ int main(int argc, char *argv[])
 	SDL_Rect RBlockT;
 	SDL_Rect RBlockZ;
 	SDL_Rect REdges;
-	
-	
+
+
 	//Dimension der Textur
 	SDL_QueryTexture(texBlockI, NULL, NULL, &RBlockI.w, &RBlockI.h);
 	SDL_QueryTexture(texBlockL, NULL, NULL, &RBlockL.w, &RBlockL.h);
@@ -153,7 +154,7 @@ int main(int argc, char *argv[])
 	SDL_QueryTexture(texBlockT, NULL, NULL, &RBlockT.w, &RBlockT.h);
 	SDL_QueryTexture(texBlockZ, NULL, NULL, &RBlockZ.w, &RBlockZ.h);
 	SDL_QueryTexture(texEdges, NULL, NULL, &REdges.w, &REdges.h);
-	
+
 	// Bewegungsrichtungen
 	int up = 0;
 	int down = 0;
@@ -163,18 +164,18 @@ int main(int argc, char *argv[])
 	int ButtonE = 0;
 	int Gravity = 0;
 	int Level = 1;
-	
+
 	// Schliessen auf false (0)
 	int running = 1;
-	
-	/*  // Controller 
+
+	/*  // Controller
 	SDL_Joystick* joystick = SDL_JoystickOpen(0);
 	printf("Controller Name: %s\n", SDL_JoystickName(joystick));
 	printf("test");
 	printf("Num Axes: %d\n", SDL_JoystickNumAxes(joystick));
 	printf("Num Buttons: %d\n", SDL_JoystickNumButtons(joystick));
 	*/
-	
+
 	/* Animation */
 	while (running)
 	{
@@ -185,7 +186,7 @@ int main(int argc, char *argv[])
 				{
 					running = 0;
 				}
-				
+
 				else if (event.type == SDL_KEYDOWN)
 				{
 					switch (event.key.keysym.scancode)
@@ -223,7 +224,7 @@ int main(int argc, char *argv[])
 							break;
 					}
 				}
-				
+
 				else if (event.type == SDL_KEYUP)
 				{
 					switch (event.key.keysym.scancode)
@@ -255,24 +256,24 @@ int main(int argc, char *argv[])
 					}
 				}
 		}
-		
-		
-		
+
+
+
 	/*	if (up && !down) */
 		if (down && !up)
 		{
 			tetrisApplyGravity(pointGame);
 		}
-		
-		if (left && !right) 
+
+		if (left && !right)
 		{
 			//tetrisMoveLeft(pointGame);
 		}
-		
-		if (right && !left) 
+
+		if (right && !left)
 		{
 			//tetrisMoveRight(pointGame);
-		}	
+		}
 		if (ButtonQ)
 		{
 			//tetrisTurnBlockLeft(pointGame);
@@ -282,7 +283,7 @@ int main(int argc, char *argv[])
 			//tetrisTurnBlockRight(pointGame);
 		}
 
-	
+
 		/* Gravity */
 		if (Gravity >= FPS)
 		{
@@ -290,55 +291,55 @@ int main(int argc, char *argv[])
 			Gravity = 0;
 		}
 
-		
+
 		// Leere das Fenster
 		SDL_RenderClear(rend);
-		
+
 		/* Neue Texturen auf die Map */
 		for (int x = 0; x < game.columns; x++){
 			for (int y = 0; y < game.rows; y++){
 				renderMap[y][x] = game.map[y][x];
 			}
 		}
-		
+
 		for (int i = 0; i < 4; i++){
 			int checkX = tetrisGetArrayInfo(game.falling.type, game.falling.orientation, i).x + game.falling.pos.x;
 			int checkY = tetrisGetArrayInfo(game.falling.type, game.falling.orientation, i).y + game.falling.pos.y;
 			renderMap[checkY][checkX] = game.falling.type + 1;
 		}
-		
+
 		/* Background */
 		SDL_RenderCopy(rend, texBackgroundImage, NULL, NULL);
-		
-		
+
+
 		/* Edges Start */
 		for (int y = 0; y < game.rows; y++)
-		{ 
+		{
 			REdges.x = -1 * PpB + (RESOLUTION_WIDTH/2) - 5*PpB;
 			REdges.y = y * PpB + 3*PpB;
 				SDL_RenderCopy(rend, texEdges, NULL, &REdges);
 		}
 		for (int y = 0; y < game.rows; y++)
-		{ 
+		{
 			REdges.x = game.columns * PpB + (RESOLUTION_WIDTH/2) - 5*PpB;
 			REdges.y = y * PpB + 3*PpB;
 				SDL_RenderCopy(rend, texEdges, NULL, &REdges);
 		}
 		for (int x = -1; x < (game.columns + 1); x++)
-		{ 
+		{
 			REdges.x = x * PpB + (RESOLUTION_WIDTH/2) - 5*PpB;
 			REdges.y = -1 * PpB + 3*PpB;
 				SDL_RenderCopy(rend, texEdges, NULL, &REdges);
 		}
 		for (int x = -1; x < (game.columns + 1); x++)
-		{ 
+		{
 			REdges.x = x * PpB + (RESOLUTION_WIDTH/2) - 5*PpB;
 			REdges.y = game.rows * PpB + 3*PpB;
 				SDL_RenderCopy(rend, texEdges, NULL, &REdges);
 		}
 		/* Edges Ende */
-		
-			
+
+
 		for (int x = 0; x < game.columns; x++)
 		{
 			for (int y = 0; y < game.rows; y++)
@@ -375,22 +376,22 @@ int main(int argc, char *argv[])
 								SDL_RenderCopy(rend, texBlockZ, NULL, &RBlockZ);
 							break;
 					default: break;
-				} 
+				}
 			}
 		}
 		tetrisCheckLines(pointGame);
 		SDL_RenderPresent(rend);
-		
-		
+
+
 		/* On every Frame */
 		SDL_Delay(1000/FPS);
 		Gravity+= Level;
-			
-		
-		
-		
+
+
+
+
 	} /* Animation Ende */
-	
+
 	/* Resourcen schliessen */
 	SDL_DestroyTexture(texBlockI);
 	SDL_DestroyTexture(texBlockL);
@@ -404,6 +405,6 @@ int main(int argc, char *argv[])
 	SDL_DestroyRenderer(rend);
 	SDL_DestroyWindow(win);
 	SDL_Quit();
-	
+
 	return 0;
 }
